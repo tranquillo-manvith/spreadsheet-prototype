ed75717ebab1f30ab1acd4c2baa0f481333fd3cc
tranquillo_manvith <manvithpoojary095@gmail.com>
2025-07-02 14:24:05 +0530

    Initial Commit: setup Vite + React + Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-emerald-500 text-white text-3xl font-bold p-6 text-center">
+      Tailwind with Vite plugin is working 🎉
+    </div>
+  );
+}
+
+export default App;
