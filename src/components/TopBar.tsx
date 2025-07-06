43aecefd4961d9a377e1ee0b947ac03c1da675e2
tranquillo_manvith <manvithpoojary095@gmail.com>
2025-07-06 20:33:02 +0530

    TopBar Pixel Prefcted

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,24 +1,23 @@
 import { type JSX } from "react";
-import WorkspaceLogo from "../assets/workspace_logo.svg";
-import Chevron from "../assets/chevron.svg";
-import ThreeDots from "../assets/three_dots.svg";
-import SearchIcon from "../assets/search_icon.svg";
-import NotificationBell from "../assets/notification_bell.svg";
-import Pfp from "../assets/pfp.png";
+import WorkspaceLogo from "../assets/top_bar/workspace_logo.svg";
+import Chevron from "../assets/top_bar/chevron.svg";
+import ThreeDots from "../assets/top_bar/three_dots.svg";
+import SearchIcon from "../assets/top_bar/search_icon.svg";
+import NotificationBell from "../assets/top_bar/notification_bell.svg";
+import Pfp from "../assets/top_bar/pfp.png";
 
 function TopBar(): JSX.Element {
   return (
-    <div className="text-[14px] font-[500] items-center justify-between border-b border-[#EEEEEE] flex flex-row w-full h-[56px] px-[16px] py-[8px] border">
-      <div className="flex flex-row items-center h-[24px] w-[343px] gap-[16px]">
-        <div className="panel">
-          <img src={WorkspaceLogo} alt="WorkspaceLogo"></img>
-        </div>
-        <div className="breadcrumbs flex flex-row items-center gap-[4px]">
+    <div className=" items-center justify-between box-border border-b-[1px] border-b-[#EEEEEE] flex flex-row w-full h-[56px] px-[16px]">
+      <div className="flex gap-[16px]">
+        <img src={WorkspaceLogo} alt="WorkspaceLogo" className=""></img>
+
+        <div className="breadcrumbs text-[14px] font-[500] flex flex-row items-center gap-[4px]">
           <div className="text-[#AFAFAF]">Workspace</div>
           <img src={Chevron} alt="Chevron" className="h-[12px] w-[12px]"></img>
           <div className="text-[#AFAFAF]">Folder 2</div>
           <img src={Chevron} alt="Chevron" className="h-[12px] w-[12px]"></img>
-          <div className="breadcrumbs flex flex-row items-center gap-[8px]">
+          <div className="breadcrumbs text-[14px] font-[500] flex flex-row items-center gap-[8px]">
             <div className="text-[#121212]">Spreadsheet 3</div>
             <img src={ThreeDots} alt="more"></img>
           </div>
@@ -32,13 +31,13 @@ function TopBar(): JSX.Element {
             Search within sheet
           </div>
         </div>
-        <div>
-          <img
-            src={NotificationBell}
-            alt="notifications"
-            className="cursor-pointer"
-          ></img>
-        </div>
+
+        <img
+          src={NotificationBell}
+          alt="notifications"
+          className="cursor-pointer"
+        ></img>
+
         <div className=" flex items-center profileBlock gap-[8px] py-[6px] pl-[8px] pr-[12px]">
           <img
             src={Pfp}
@@ -50,7 +49,7 @@ function TopBar(): JSX.Element {
             <div className="absolute flex top-0 left-0 font-[400] text-[12px] text-[#121212] h-[16px] w-[56px]">
               John Doe
             </div>
-            <div className="absolute flex bottom-0 left-0 font-[400] text-[10px] text-[#757575] h-[12px] w-[56px]">
+            <div className="absolute left-0 bottom-[1px] font-[400] text-[10px] text-[#757575] h-[12px] w-[56px]">
               john.doe...
             </div>
           </div>
